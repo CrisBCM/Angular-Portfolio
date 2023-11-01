ba49dc01c55fa5f0fe36e1b0b31d83208bef76f2
Cristian <cristianbcm1999@gmail.com>
2023-10-31 22:38:44 -0300

    Feat: add header with responsive navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuNavbarComponent } from './components/menu-navbar/menu-navbar.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFile } from '@fortawesome/free-regular-svg-icons';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary){
+    library.addIcons(faFile);
+  }
+}
